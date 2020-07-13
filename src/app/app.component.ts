ae1fe1cab4afab7f9d050d30c04235792f7a6515
Di <divasilevski@gmail.com>
2020-07-13 18:26:03 +0300

    ngSwitch ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  toggle = false;
+  toggle: any = false;
+  arr = [1, 1, 2, 3, 5, 8 ,13];
+
+  objs = [
+    {title: 'Post 1', author: 'Dima', comments: [
+      {name: 'Max', text: 'lorem 1'},
+      {name: 'Max', text: 'lorem 2'},
+      {name: 'Max', text: 'lorem 3'},
+    ]},
+    {title: 'Post 2', author: 'Dima2', comments: [
+      {name: 'Max2', text: 'lorem 1'},
+      {name: 'Max2', text: 'lorem 2'},
+      {name: 'Max2', text: 'lorem 3'},
+    ]}
+  ]
 }
