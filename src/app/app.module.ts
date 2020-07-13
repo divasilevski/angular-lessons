38782d24bdf6d76877b53762b46e808fcfba728e
Di <divasilevski@gmail.com>
2020-07-13 13:52:33 +0300

    template and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { AuthorComponent } from './post/author/author.component';
+import { Post2Component } from './post2/post2.component';
+import { Post3Component } from './post3/post3.component';
+import { Post4Component } from './post4/post4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-    AuthorComponent
+    AuthorComponent,
+    Post2Component,
+    Post3Component,
+    Post4Component
   ],
   imports: [
     BrowserModule
