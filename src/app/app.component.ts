3eb8eb2b4482d5ee309f13b5515c0f2f49fecfb1
Di <divasilevski@gmail.com>
2020-07-13 15:50:23 +0300

    ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,31 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  title = 'Dynamic title';
-  number = 42;
-  array = [1, 2, 3];
+  title = 'initial';
 
-  obj = { a: 1, b: { c: 2 } };
-  img = 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png';
-  img2 = 'https://learn.javascript.ru/courses/list/angular.png';
-
-  inputValue = '';
-
-  constructor() {
-    // setTimeout(() => {
-    //   this.img = this.img2;
-    // }, 5000)
-  }
-
-  onInput(event: KeyboardEvent) {
-    this.inputValue = (<HTMLInputElement>event.target).value;
-  }
-
-  onClick() {
-    this.inputValue = '';
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str;
+  onInput(event) {
+    this.title = event.target.value;
   }
 }
