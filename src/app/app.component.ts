f928466f2a53917c38ff09d482a5de7f5c7a70b9
Di <divasilevski@gmail.com>
2020-07-13 18:30:54 +0300

    pipes small

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  toggle: any = false;
-  arr = [1, 1, 2, 3, 5, 8 ,13];
-
-  objs = [
-    {title: 'Post 1', author: 'Dima', comments: [
-      {name: 'Max', text: 'lorem 1'},
-      {name: 'Max', text: 'lorem 2'},
-      {name: 'Max', text: 'lorem 3'},
-    ]},
-    {title: 'Post 2', author: 'Dima2', comments: [
-      {name: 'Max2', text: 'lorem 1'},
-      {name: 'Max2', text: 'lorem 2'},
-      {name: 'Max2', text: 'lorem 3'},
-    ]}
-  ]
+  now: Date = new Date();
 }
