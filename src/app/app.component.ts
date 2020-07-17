8cdcfcfd1a1ac3d583dd20986016355a601052b9
Di <divasilevski@gmail.com>
2020-07-17 19:14:40 +0300

    form for validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,11 @@
-import { Component } from '@angular/core';
-import { Subject, Subscription } from 'rxjs';
-
-export interface Post {
-  title: string;
-  text: string;
-}
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: []
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = "RxJS";
 
-  sub: Subscription;
-
-  stream$: Subject<number> = new Subject<number>();
-  counter = 0;
-
-  constructor(){
-    this.sub = this.stream$
-      .subscribe(value => console.log('sub', value))
-  
-  }
-
-  next() {
-    this.counter++;
-    this.stream$.next(this.counter);
-  }
-
-  stop() {
-    this.sub.unsubscribe();
-  }
 }
+
