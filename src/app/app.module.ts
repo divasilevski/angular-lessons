e5666a67cd5693c0d78c7dfcc474843abff4580c
Di <divasilevski@gmail.com>
2020-07-17 14:38:46 +0300

    service connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    CounterComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/services/app-counter.service.ts
+++ src/app/services/app-counter.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { LogService } from './log.service'
 
 @Injectable({
   providedIn: 'root'
@@ -6,11 +7,17 @@ import { Injectable } from '@angular/core';
 export class AppCounterService {
   counter = 0;
 
+  constructor(private logService: LogService) {
+
+  }
+
   increase() {
+    this.logService.log("+++");
     this.counter++;
   }
 
   decrease() {
+    this.logService.log("---");
     this.counter--;
   }
 }
