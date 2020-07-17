8ae459ae5c64ef6ed5680941e3d5760700b253b8
Di <divasilevski@gmail.com>
2020-07-17 14:24:43 +0300

    local service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppCounterService } from './services/app-counter.service'
 
 import { AppComponent } from './app.component';
 
@@ -14,9 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [
-    AppCounterService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
