22f81d0867fc9968fa6dad64333325c4e847f1c6
Di <divasilevski@gmail.com>
2020-07-18 12:19:06 +0300

    validation form and control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
-import {Component} from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { FormGroup, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form: FormGroup;
 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(null)
+    });
+  }
+
+  submit() {
+    console.log('Form', this.form);
+
+    const formData = { ...this.form.value }
+    console.log('Form', formData);
+
+  }
 }
 
