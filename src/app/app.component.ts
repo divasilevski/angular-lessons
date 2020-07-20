a649b18d3a052de0c50d5c85a002a068d1d3d480
Di <divasilevski@gmail.com>
2020-07-20 13:47:13 +0300

    HttpClientModule and get request 'todos'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { HttpClient } from '@angular/common/http';
 
-
+export interface Todo {
+  complited: boolean;
+  title: string;
+  id?: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  todos: Array<Todo> = [];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get<Array<Todo>>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe(todos => {
+        console.table(todos);
+        this.todos = todos;
+      })
+
+  }
+}
 
