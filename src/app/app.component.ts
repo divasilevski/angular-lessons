8e3deb80e7174eeb0e38ed036768b0e833b569e4
Di <divasilevski@gmail.com>
2020-07-20 15:01:04 +0300

    post method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,18 +14,34 @@ export interface Todo {
 export class AppComponent implements OnInit {
 
   todos: Array<Todo> = [];
+  todoTitle = '';
 
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {  }
 
   ngOnInit(): void {
+    this.fetchTodos();
+  }
+
+  addTodo() {
+    if (!this.todoTitle.trim()) return;
+
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      complited: false
+    }
+
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe(todo => {
+        this.todos.push(todo);
+      })
+  }
+
+  fetchTodos() {
     this.http.get<Array<Todo>>('https://jsonplaceholder.typicode.com/todos?_limit=2')
       .subscribe(todos => {
         console.table(todos);
         this.todos = todos;
       })
-
   }
 }
 
