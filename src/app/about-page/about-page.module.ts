0b59848e162eb3baf4a676e7a20ec788c691c497
Di <divasilevski@gmail.com>
2020-07-22 19:46:52 +0300

    few modules

---
+++ src/app/about-page/about-page.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+
+import { AboutPageComponent } from './about-page.component';
+import { AboutExtraPageComponent } from './about-extra-page/about-extra-page.component';
+import { SharedModule } from '../shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    CommonModule,
+    RouterModule.forChild([
+      {path: 'about', component: AboutPageComponent, children: [
+          { path: 'extra', component: AboutExtraPageComponent }
+        ]},
+    ])
+  ],
+  declarations: [
+    AboutPageComponent,
+    AboutExtraPageComponent,
+  ],
+  exports: [RouterModule],
+})
+export class AboutPageModule {
+
+}
