81e9331f439a2b7589fb7c4a43d31573d346beb3
Di <divasilevski@gmail.com>
2020-07-24 19:12:17 +0300

    the integration test with injection

--- src/app/posts/posts.component.spec.ts
+++ src/app/posts/posts.component.spec.ts
@@ -1,13 +1,36 @@
-import {PostsComponent} from "./posts.component";
-import {PostsService} from "./posts.service";
+import { PostsComponent } from "./posts.component";
+import { PostsService } from "./posts.service";
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('PostsComponent', () => {
+  let fixture: ComponentFixture<PostsComponent>
   let component: PostsComponent
   let service: PostsService
 
   beforeEach(() => {
-    service = new PostsService(null)
-    component = new PostsComponent(service)
+    TestBed.configureTestingModule({
+      declarations: [PostsComponent],
+      providers: [PostsService],
+      imports: [HttpClientModule]
+    })
+
+    fixture = TestBed.createComponent(PostsComponent)
+    component = fixture.componentInstance
+
+    // service = fixture.debugElement.injector.get(PostsService) // or
+    service = TestBed.get(PostsService)
+  })
+
+
+  it('should fetch posts on ngOnInit', () => {
+    const posts = [1, 2, 3]
+    spyOn(service, 'fetch').and.returnValue(of(posts))
+
+    fixture.detectChanges()
+
+    expect(component.posts).toEqual(posts)
   })
 
 })
