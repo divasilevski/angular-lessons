02762ce42245f6078cb2fd6ef9f962640cc20433
Di <divasilevski@gmail.com>
2020-07-24 18:48:11 +0300

    integration tests for counter component

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,5 +1,6 @@
 import {CounterComponent} from "./counter.component";
 import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 
 describe('CounterComponent', () => {
@@ -19,6 +20,35 @@ describe('CounterComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeDefined()
+  })
+
+  it('should render counter property', () => {
+    component.counter = 42
+
+    fixture.detectChanges()
+
+    let de = fixture.debugElement.query(By.css('.counter'))
+    let el: HTMLElement = de.nativeElement
+
+    expect(el.textContent).toContain('42')
+  })
+
+  it('should add green class if counter is even', () => {
+    component.counter = 6
+
+    fixture.detectChanges()
+
+    let de = fixture.debugElement.query(By.css('.counter'))
+    let el: HTMLElement = de.nativeElement
+
+    expect(el.classList.contains('green')).toBeTruthy()
+  })
+
+  it('should increment conter if increment button was clecked', () => {
+    let btn = fixture.debugElement.query(By.css('#increment'))
+    btn.triggerEventHandler('click', null)
+
+    expect(component.counter).toBe(1)
   })
 
 })
