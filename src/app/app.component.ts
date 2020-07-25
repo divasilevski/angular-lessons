2de6dc8b2c74a798847a83bfae6cc97455e0592a
Di <divasilevski@gmail.com>
2020-07-25 17:04:39 +0300

    group query keyframes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, group, query, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -20,19 +20,29 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       transition('start => end', animate(450)),
       transition('end => start', animate('800ms ease-in-out')),
       transition('special <=> *', [
-        style({ background: 'green' }),
-        animate('1s', style({
-          background: 'pink'
-        })),
-        animate(750)
+        group([
+          query('h4', animate(1500, style({
+            fontSize: '.5rem'
+          }))),
+          style({ background: 'green' }),
+          animate('1s', style({
+            background: 'pink'
+          })),
+          animate(750)
+        ])
+        
       ]),
       // transition('void => *', [
       //   style({ opacity: 0 }),
       //   animate('850ms ease-out')
       // ]),
       transition(':enter', [
-        style({ opacity: 0 }),
-        animate('850ms ease-out')
+        animate('4s', keyframes([
+          style({background: 'red', offset: 0}),
+          style({background: 'black', offset: 0.2}),
+          style({background: 'orange', offset: 0.3 }),
+          style({background: 'blue', offset: 1}),
+        ]))
       ]),
       // transition('* => void', [
       //   style({ opacity: 1 }),
@@ -43,10 +53,16 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       // ]),
       transition(':leave', [
         style({ opacity: 1 }),
-        animate(750, style({
-          opacity: 0,
-          transform: 'scale(1.2)'
-        }))
+        group([ // sequence as default
+          animate(750, style({
+            opacity: 0,
+            transform: 'scale(1.2)'
+          })),
+          animate(300, style({
+            color: '#000',
+            fontWeight: 'bold'
+          }))
+        ])
       ])
     ])
   ]
@@ -57,5 +73,13 @@ export class AppComponent {
 
   animate() {
     this.boxState = this.boxState === 'end' ? 'start' : 'end';
+  }
+
+  animationStarted(event: AnimationEvent) {
+    console.log('animationStarted', event)
+  }
+
+  animationDone(event: AnimationEvent) {
+    console.log('animationDone', event)
   }
 }
