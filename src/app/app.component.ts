64fc6690ffb13f61928e20cb96db126b273eaa52
Di <divasilevski@gmail.com>
2020-07-26 15:12:51 +0300

    create new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,85 +1,10 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate, group, query, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('box', [
-      state('start', style({ background: 'blue' })),
-      state('end', style({
-        background: 'red',
-        transform: 'scale(1.2)'
-      })),
-      state('special', style({
-        background: 'orange',
-        transform: 'scale(0.5)',
-        borderRadius: '50%'
-      })),
-      transition('start => end', animate(450)),
-      transition('end => start', animate('800ms ease-in-out')),
-      transition('special <=> *', [
-        group([
-          query('h4', animate(1500, style({
-            fontSize: '.5rem'
-          }))),
-          style({ background: 'green' }),
-          animate('1s', style({
-            background: 'pink'
-          })),
-          animate(750)
-        ])
-        
-      ]),
-      // transition('void => *', [
-      //   style({ opacity: 0 }),
-      //   animate('850ms ease-out')
-      // ]),
-      transition(':enter', [
-        animate('4s', keyframes([
-          style({background: 'red', offset: 0}),
-          style({background: 'black', offset: 0.2}),
-          style({background: 'orange', offset: 0.3 }),
-          style({background: 'blue', offset: 1}),
-        ]))
-      ]),
-      // transition('* => void', [
-      //   style({ opacity: 1 }),
-      //   animate(750, style({
-      //     opacity: 0,
-      //     transform: 'scale(1.2)'
-      //   }))
-      // ]),
-      transition(':leave', [
-        style({ opacity: 1 }),
-        group([ // sequence as default
-          animate(750, style({
-            opacity: 0,
-            transform: 'scale(1.2)'
-          })),
-          animate(300, style({
-            color: '#000',
-            fontWeight: 'bold'
-          }))
-        ])
-      ])
-    ])
-  ]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  boxState = 'end'
-  visible = true
-
-  animate() {
-    this.boxState = this.boxState === 'end' ? 'start' : 'end';
-  }
-
-  animationStarted(event: AnimationEvent) {
-    console.log('animationStarted', event)
-  }
-
-  animationDone(event: AnimationEvent) {
-    console.log('animationDone', event)
-  }
+  title = 'AngularLessons';
 }
