45a53785d7be901bbb328c50b083f1f87119548a
Di <divasilevski@gmail.com>
2020-07-29 13:08:48 +0300

    dashboard page

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -23,4 +23,19 @@ export class PostsService {
       }))
   }
 
+
+  getAll() {
+    return this.http.get(`${environment.fbDbUrl}/posts.json`)
+      .pipe(map((response: { [key: string]: any }) => {
+        return Object
+          .keys(response)
+          .map(key => ({
+            ...response[key],
+            id: key,
+            data: new Date(response[key].date)
+          }))
+        
+      }))
+  }
+
 }
