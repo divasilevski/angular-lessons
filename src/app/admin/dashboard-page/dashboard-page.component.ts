dd3f0d424538761ce160533baa76f60fa95d180a
Di <divasilevski@gmail.com>
2020-07-29 13:21:12 +0300

    remove post

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -12,6 +12,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = []
   pSub: Subscription
+  dSub: Subscription
   searchStr = ''
 
   constructor(
@@ -28,10 +29,16 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     if (this.pSub) {
       this.pSub.unsubscribe()
     }
+
+    if (this.dSub) {
+      this.dSub.unsubscribe()
+    }
   }
 
   remove(id: string) {
-
+    this.dSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter( post => post.id !== id)
+    })
   }
 
 }
